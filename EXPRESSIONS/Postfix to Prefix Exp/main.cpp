17c4f60162529ce9b77314a64c78515f2c7f232a
JordanPooleEffect <zmtabinas@gmail.com>
2023-12-10 12:51:53 +0800

    EXPRESSION - FOR FINALS

---
+++ EXPRESSIONS/Postfix to Prefix Exp/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include "arraystack.h"
+using namespace std;
+
+int main() {
+	Stack* stack = new ArrayStack();
+    string input;
+    do {
+    	cout << "Enter: ";
+    	cin >> input;
+    	int num;
+    	string a, b;
+    	switch (input[0]) {
+            // TODO add cases for operations here
+    		case 'x':
+    			cout << "Prefix: " << stack->pop() << endl;
+    			break;
+
+            case '+':
+                a = stack->pop();
+                b = stack->pop();
+                stack->push("+ " + b + " " + a);
+                break;
+            case '-':
+                a = stack->pop();
+                b = stack->pop();
+                stack->push("- " + b + " " + a);
+                break;
+            case '*':
+                a = stack->pop();
+                b = stack->pop();
+                stack->push("* " + b + " " + a);
+                break;
+            case '/':
+                a = stack->pop();
+                b = stack->pop();
+                stack->push("/ " + b + " " + a);
+                break;
+
+    		default: // number
+    			stack->push(input);
+    			break;
+		}
+	} while (input != "x");
+    return 0;
+}
